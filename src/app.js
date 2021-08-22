857442c2e4820d137a32e9d302cb22c26b86d02f
gabriel6181997 <gabriel6181997@gmail.com>
2021-08-22 17:04:09 +0800

    ✨ Add queries

--- src/app.js
+++ src/app.js
@@ -34,11 +34,30 @@ app.get("/help", (req, res) => {
 });
 
 app.get("/weather", (req, res) => {
+  if(!req.query.address){
+    return res.send({
+      error: "You must provide an address"
+    })
+  }
+
   res.send({
     forecast: "It is sunny",
     location: "Hong Kong",
+    address: req.query.address
   });
 });
+
+app.get('/products', (req, res) => {
+  if(!req.query.search){
+    return res.send({
+      error: "You must provide a search term"
+    })
+  }
+
+  res.send({
+    products: [],
+  })
+})
 
 app.get("/help/*", (req, res) => {
   res.render("404",{
