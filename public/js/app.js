9b87caa9069c8b5fb8d81097091551ec68a18e33
gabriel6181997 <gabriel6181997@gmail.com>
2021-08-22 21:52:34 +0800

    ✨ Add fetch function

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,12 @@
-console.log('Client side JavaScript file is loaded!')
+console.log("Client side JavaScript file is loaded!");
+
+fetch("http://localhost:3000/weather?address=boston").then((response) => {
+  response.json().then((data) => {
+    if(data.error) {
+      console.log(data.error);
+    } else {
+      console.log(data.location);
+      console.log(data.forecast);
+    }
+  });
+});
