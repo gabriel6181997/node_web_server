c84d810e0e153b3bf159b32ed3f7281b2a735e87
gabriel6181997 <gabriel6181997@gmail.com>
2021-08-22 15:05:01 +0800

    ♻️ Create and use header and footer

--- src/app.js
+++ src/app.js
@@ -1,15 +1,18 @@
 const path = require("path");
 const express = require("express");
+const hbs = require("hbs");
 
 const app = express();
 
 // Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, "../public");
-const viewsPath = path.join(__dirname, "../templates");
+const viewsPath = path.join(__dirname, "../templates/views");
+const partialsPath = path.join(__dirname, "../templates/partials");
 
 // Setup handlebars engine and views location
 app.set("view engine", "hbs");
 app.set("views", viewsPath);
+hbs.registerPartials(partialsPath);
 
 // Setup static directory to serve
 app.use(express.static(publicDirectoryPath));
@@ -19,11 +22,15 @@ app.get("", (req, res) => {
 });
 
 app.get("/about", (req, res) => {
-  res.render("about", { title: "About Me", name: "Gabriel Wong me" });
+  res.render("about", { title: "About Me", name: "Gabriel Wong" });
 });
 
 app.get("/help", (req, res) => {
-  res.render("help", { helpText: "This is some helpful text." });
+  res.render("help", {
+    helpText: "This is some helpful text.",
+    title: "Help",
+    name: "Gabriel Wong",
+  });
 });
 
 app.get("/weather", (req, res) => {
