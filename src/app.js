0be97381871eab460189f01df22314763d01789a
gabriel6181997 <gabriel6181997@gmail.com>
2021-09-05 17:43:24 +0800

    🚀 Change environment setting for Heroku deployment

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const geocode = require("./utils/geocode");
 const forecast = require("./utils/forecast");
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 // Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, "../public");
@@ -57,7 +58,7 @@ app.get("/weather", (req, res) => {
         res.send({
           forecast: forecastData,
           location,
-          address: req.query.address
+          address: req.query.address,
         });
       });
     }
@@ -92,6 +93,6 @@ app.get("*", (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log("Server is up on port 3000");
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}`);
 });
